0debe3cd3bd01e7d235a451dd75fe583c21d27ea
Wanfranklin Alves <wanfranklin@gmail.com>
2023-10-17 00:30:35 -0300

    Inclusão de comentários

--- Infrastructure/Interfaces/IClienteRepository.cs
+++ Infrastructure/Interfaces/IClienteRepository.cs
@@ -2,12 +2,22 @@
 
 namespace Infrastructure.Interfaces
 {
+    // Definição da interface IClienteRepository
     public interface IClienteRepository
     {
+        // Método para obter todos os clientes
         Task<List<Cliente>> ObterTodosClientes();
+
+        // Método para obter um cliente por ID
         Task<Cliente> ObterClientePorId(Guid id);
+
+        // Método para adicionar um cliente
         Task AdicionarCliente(Cliente cliente);
+
+        // Método para atualizar um cliente por ID
         Task AtualizarCliente(Guid id, Cliente cliente);
+
+        // Método para excluir um cliente por ID
         Task ExcluirCliente(Guid id);
     }
 }
